01f6eae54ba422a6f79e3357e172da208c945fc1
anton1113 <anton11131113@gmail.com>
2018-03-27 17:18:58 +0300

    removed asci added timestamp

--- src/main/java/com/arash/spring/demo/rest/DemoController.java
+++ src/main/java/com/arash/spring/demo/rest/DemoController.java
@@ -2,14 +2,15 @@ package com.arash.spring.demo.rest;
 
 import com.arash.spring.demo.model.Request;
 import com.arash.spring.demo.model.Response;
-import com.arash.spring.demo.service.AsciService;
 import com.arash.spring.demo.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
@@ -19,21 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @Autowired private DemoService demoService;
-    @Autowired private AsciService asciService;
 
     @RequestMapping(method = RequestMethod.GET, value = "/rest/say-hello")
     public String sayHello() {
         return "Hello, friend!";
     }
 
+    @RequestMapping(method = RequestMethod.GET, value = "/rest/timestamp")
+    public String getTimeStamp() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS");
+        return sdf.format(new Date());
+    }
+
     @RequestMapping(method = RequestMethod.POST, value = "/rest/demo")
     public Response serve(@RequestBody Request request) {
         return demoService.serve(request);
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "/rest/asci")
-    public ResponseEntity<byte[]> getDownloadData() throws Exception {
-        return asciService.fetchAsci();
     }
 
 }
