f9a7b00a60c592ca0a19b302743ea040fa02be14
anton1113 <anton11131113@gmail.com>
2018-03-29 12:12:24 +0300

    run branch

--- jenkins_pipelines/run_branch.groovy
+++ jenkins_pipelines/run_branch.groovy
@@ -10,13 +10,14 @@ node('master') {
     }
 
     def branchName = userInput['branch name']
+    println branchName
 
     stage('Git checkout') {
         checkout([$class: 'GitSCM', branches: [[name: '*/' + branchName]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
                   userRemoteConfigs: [[credentialsId: 'e404b1e5-89f9-4f62-ade7-aa5bcd182b55', url: 'https://anton1113@github.com/anton1113/demo.git']]])
     }
 
-    def core = load "jenkins_pipelines/core.groovy"
-    core.build()
-    core.restart()
+//    def core = load "jenkins_pipelines/core.groovy"
+//    core.build()
+//    core.restart()
 }
