92a9486b630abfa50b7de2f30f9b54f79310a23e
anton1113 <anton11131113@gmail.com>
2018-03-29 12:29:24 +0300

    msg

--- jenkins_pipelines/run_branch.groovy
+++ jenkins_pipelines/run_branch.groovy
@@ -7,18 +7,17 @@ node('master') {
                 id: 'userInput', message: 'Please enter the branch name', parameters: [
                 [$class: 'TextParameterDefinition', description: 'Name of the branch to build', name: 'branch name']
         ])
-        echo ("Branch name: " + userInput['branch name'])
+        echo ("Branch name: " + userInput)
     }
 
-    def branchName = userInput['branch name']
-    sh 'echo ' + branchName
+    def branchName = userInput
 
-//    stage('Git checkout') {
-//        checkout([$class: 'GitSCM', branches: [[name: '*/' + branchName]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
-//                  userRemoteConfigs: [[credentialsId: 'e404b1e5-89f9-4f62-ade7-aa5bcd182b55', url: 'https://anton1113@github.com/anton1113/demo.git']]])
-//    }
-//
-//    def core = load "jenkins_pipelines/core.groovy"
-//    core.build()
-//    core.restart()
+    stage('Git checkout') {
+        checkout([$class: 'GitSCM', branches: [[name: '*/' + branchName]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
+                  userRemoteConfigs: [[credentialsId: 'e404b1e5-89f9-4f62-ade7-aa5bcd182b55', url: 'https://anton1113@github.com/anton1113/demo.git']]])
+    }
+
+    def core = load "jenkins_pipelines/core.groovy"
+    core.build()
+    core.restart()
 }
