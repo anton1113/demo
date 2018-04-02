c4fcc9e7f3f40c2b2ad2068272cd9395bd8fa649
anton1113 <anton11131113@gmail.com>
2018-04-02 12:37:38 +0300

    rm

--- jenkins_pipelines/SparseCheckout.groovy
+++ jenkins_pipelines/SparseCheckout.groovy
@@ -5,7 +5,7 @@ node('master') {
     sh 'ls -1'
 
     stage('Git checkout') {
-        checkout([$class: 'GitSCM', branches: [[name: '92a9486']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
+        checkout([$class: 'GitSCM', branches: [[name: 'c04697c']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
                   userRemoteConfigs: [[credentialsId: 'e404b1e5-89f9-4f62-ade7-aa5bcd182b55', url: 'https://anton1113@github.com/anton1113/demo.git']],
                   sparseCheckoutPaths: [['jenkins_pipelines/']]])
     }
