f336c45dfffe57c27b9edc7738260dd1f07db425
anton1113 <anton11131113@gmail.com>
2018-04-02 11:00:58 +0300

    ls -1

--- jenkins_pipelines/SparseCheckout.groovy
+++ jenkins_pipelines/SparseCheckout.groovy
@@ -2,6 +2,14 @@ node('master') {
 
     sh 'pwd'
     sh 'ls -1'
+
+    stage('Git checkout') {
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [],
+                  userRemoteConfigs: [[credentialsId: 'e404b1e5-89f9-4f62-ade7-aa5bcd182b55', url: 'https://anton1113@github.com/anton1113/demo.git']]])
+    }
+
+    sh 'pwd'
+    sh 'ls -1'
     def core = load "jenkins_pipelines/JenkinsCore.groovy"
     core.foo()
 }
