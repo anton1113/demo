a86c5927647a73942dd724a7a9424d09de9301bc
anton1113 <anton11131113@gmail.com>
2018-04-04 16:07:27 +0300

    groovy build plugin

--- jenkins_pipelines/JenkinsCore.groovy
+++ jenkins_pipelines/JenkinsCore.groovy
@@ -7,14 +7,7 @@ def build() {
     }
 
     stage('Transfer jars') {
-
-        def currDate = new Date().format("yyy-MM-dd:")
-        def lastCommitName = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
-        def currBuildFolderName = currDate + lastCommitName
-
         sh 'scp target/*.jar root@80.211.135.72:/var/lib/demo'
-        ssh('mkdir /var/lib/demo/' + currBuildFolderName)
-        ssh('cp /var/lib/demo/demo-0.jar /var/lib/demo/' + currBuildFolderName)
     }
 }
 
@@ -23,27 +16,6 @@ def restart() {
     stage('Restart service') {
         sh 'ssh root@80.211.135.72 \'systemctl restart demo\''
     }
-}
-
-def getCommitHashAndDate() {
-
-    stage('Commit hash/date input') {
-        def userInput = input(
-                id: 'userInput', message: 'Please enter the parameters', parameters: [
-                [$class: 'TextParameterDefinition', description: 'Hash of the desired commit', name: 'commit hash'],
-                [$class: 'TextParameterDefinition', description: 'Date of the desired build (dd-MM-yyyy)', name: 'build date']
-        ])
-    }
-    return userInput
-}
-
-def ssh(command) {
-    sh 'ssh root@80.211.135.72 \'' + command + '\''
-}
-
-void foo() {
-
-    println 'foo'
 }
 
 return this
